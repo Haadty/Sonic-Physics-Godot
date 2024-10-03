468d2c0bc3b311792bb5200a63f5ad7dcd7e5e5d
Haadty <gggamerdosgames@gmail.com>
2024-10-02 20:05:07 -0400

    update

---
+++ Scripts/Player/States/air_player_state.gd
@@ -0,0 +1,32 @@
+extends PlayerState
+class_name AirPlayerState
+
+var last_absolute_horizontal_speed: float
+
+func enter(player: Player):
+	last_absolute_horizontal_speed = abs(player.velocity.x)
+	
+	if player.is_rolling:
+		player.set_bounds(1)
+
+func step(player: Player, delta: float):
+	player.handle_gravity(delta)
+	player.handle_acceleration(delta)
+	player.handle_deceleration(delta)
+
+	if player.is_grounded():
+		if player.input_direction.y < 0 and abs(player.velocity.x) > player.current_stats.min_speed_to_roll:
+			player.state_machine.change_state("Rolling")
+		else:
+			player.state_machine.change_state("Regular")
+
+	player.handle_jump()
+
+func animate(player: Player, _delta: float):
+	player.skin.handle_flip(player.input_direction.x)
+
+	if player.is_rolling:
+		player.skin.set_animation_state(PlayerSkin.ANIMATION_STATES.rolling)
+		player.skin.set_rolling_animation_speed(last_absolute_horizontal_speed)
+	elif player.state_machine.last_state == "Spring" or player.state_machine.last_state == "Braking":
+		player.skin.set_animation_state(PlayerSkin.ANIMATION_STATES.walking)
---
+++ Scripts/Zones/setup.gd
@@ -0,0 +1,31 @@
+extends Node2D
+class_name Zone
+
+@export var player_resource: PackedScene
+@export var camera_resource: PackedScene
+
+@export var limit_left: int = 0
+@export var limit_right: int = 10000
+@export var limit_top: int = 0
+@export var limit_bottom: int = 10000
+
+@onready var start_point: Node2D = $Level/StartPoint
+
+var player: Player
+var camera: PlayerCamera
+
+func _ready():
+	initialize_player()
+	initialize_camera()
+
+func initialize_player():
+	player = player_resource.instantiate()
+	player.position = start_point.position
+	player.lock_to_limits(limit_left, limit_right)
+	add_child(player)
+
+func initialize_camera():
+	camera = camera_resource.instantiate()
+	camera.set_player(player)
+	camera.set_limits(limit_left, limit_right, limit_top, limit_bottom)
+	add_child(camera)
